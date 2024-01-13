6121a0c4888f2478f8b8afe7acb98bac5636b115
xpknight <xpknight@gmail.com>
2024-01-13 22:58:37 +0530

    Async Fetch Added but Commented

--- App.js
+++ App.js
@@ -1,9 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, SafeAreaView, StyleSheet, View } from 'react-native';
 import LaunchScreen from './screens/LaunchScreen';
+import { useEffect, useState } from 'react';
+import { SplashScreen } from 'expo';
 
 export default function App() {
-  return (<>
+  const [isAppReady, setAppReady] = useState(false);
+
+  // useEffect(() => {
+  //   async function prepareApp() {
+  //     try {
+  //       // Prevent SplashScreen from auto-hiding
+  //       await SplashScreen.preventAutoHideAsync();
+
+  //       // Load your app assets here (e.g., images)
+  //       const imageAssets = [require('./assets/background.jpg')];
+  //       const cacheImages = imageAssets.map((image) =>
+  //         Image.prefetch(image)
+  //       );
+
+  //       await Promise.all(cacheImages);
+
+  //       // Indicate that the app is ready to be displayed, and hide SplashScreen
+  //       setAppReady(true);
+  //       await SplashScreen.hideAsync();
+  //     } catch (e) {
+  //       console.warn("Error in prepareApp: ", e);
+  //     }
+  //   }
+
+  //   prepareApp();
+  // }, []);
+
+  // if (!isAppReady) {
+  //   return null; // or a loading indicator
+  // }
+
+  return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <ImageBackground
@@ -12,13 +45,11 @@ export default function App() {
         source={require("./assets/background.jpg")}
         resizeMode="cover"
       >
-        {/* <SafeAreaView style={styles.startScreen}> */}
-        <LaunchScreen />
-        {/* </SafeAreaView> */}
+        <SafeAreaView style={styles.startScreen}>
+          <LaunchScreen />
+        </SafeAreaView>
       </ImageBackground>
     </View>
-  </>
-
   );
 }
 
